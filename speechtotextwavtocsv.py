6e725e084de06c4485dee06facc4d41fb46c516a
Vamsi Krishna <vamsikrishna3296@gmail.com>
2023-02-13 22:03:32 +0530

    Wav audio files to text.csv

---
+++ speechtotextwavtocsv.py
@@ -0,0 +1,62 @@
+# Import library
+import speech_recognition as sr
+from playsound import playsound
+import os
+import readline
+
+def rlinput(prompt, prefill=''):
+    readline.set_startup_hook(lambda: readline.insert_text(prefill))
+    try:
+        return input(prompt)
+    finally:
+        readline.set_startup_hook()
+
+# Name the output csv file as you wish
+path_to_meta = "./metadata.csv"
+
+
+
+# Initialize recognizer class (for recognizer he speech)
+r = sr.Recognizer()
+path_to_wav = "./wav_audio_files"
+
+# Reading Audio file as source
+# listening the audio file and store in audio_text variable
+
+for filename in os.listdir(path_to_wav):
+    if filename == ".DS_Store":
+        continue
+    file_in_wav = os.path.join(path_to_wav, filename)
+
+    print(file_in_wav)
+    with sr.AudioFile(file_in_wav) as source:
+
+        audio_text = r.listen(source)
+
+        # recognize() method will throw an error if the API is unreachable.
+        # Hence using the exception handling
+        try:
+            # Using googles speech recognition
+            text = r.recognize_google(audio_text)
+            # print('Converting audio transcripts into text...')
+            # print(text)
+
+            # while True:
+            #     playsound(path_to_wav)
+            #     # print("Please check if the audio matches the description")
+            #     repeat = input("Play audio to check the transcript matches?(y/n)")
+            #     if repeat != "y":
+            #         break
+            
+            # edit_transcript = input("Edit transcript?(y/n)")
+            
+            # if edit_transcript == "y":
+            #     text = rlinput(prompt="Edit Text: ", prefill=text)
+            with open(path_to_meta, 'a') as the_file:
+                the_file.write(filename + "|" + text + "\n")
+        except:
+            print('Sorry.. run again...')
+
+
+
+
